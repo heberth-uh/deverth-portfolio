bca67ce67e272fde8a82008a12ce5a15e6363a86
heberth-uh <heberth.uh@gmail.com>
2024-09-16 17:59:05 -0600

    FIX project image preview

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,7 +10,7 @@ export const projects_links = [
     },
     {
         name: 'quickNotes',
-        img: 'https://private-user-images.githubusercontent.com/65416264/367933527-a231fbed-d8ca-4191-923d-167121f9bd3e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjY1MjM1MTIsIm5iZiI6MTcyNjUyMzIxMiwicGF0aCI6Ii82NTQxNjI2NC8zNjc5MzM1MjctYTIzMWZiZWQtZDhjYS00MTkxLTkyM2QtMTY3MTIxZjliZDNlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA5MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwOTE2VDIxNDY1MlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTZiODQ5YmYxMjE1MDYzNjU5ODY1MDM4ZDlmYzc4ZDY3ZDljM2QwNzJkNjkzNzM4OGIwNWFhMmI3OWI2ZTI3ZTYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.qXX5WkaU1qG5n7JlxbStLhiD7axVxBi8IwIOE3qte_Y',
+        img: '../../images/projects/project-quick-notes.png',
         links: {
             preview_link: "https://quicktask.up.railway.app/",
             repository_link: "https://github.com/heberth-uh/quick-notes"
